baf1d0be8a0160aba6c684b9ea187778c0bf475a
relaxonator <38090805+paulLaxon@users.noreply.github.com>
2022-03-10 10:56:43 -0500

    Revert "Refactoring 3"

--- rentals.rb
+++ rentals.rb
@@ -1,5 +1,3 @@
-# frozen_string_literal: true
-
 # Movie class
 class Movie
   REGULAR = 0
@@ -7,23 +5,10 @@ class Movie
   CHILDRENS = 2
 
   attr_reader :title, :price_code
-  attr_writer :price
 
-  def price_code=(value)
-    @price_code = value
-  end
-
-  def initialize(title, initial_price_code)
+  def initialize(title, price_code)
     @title = title
-    self.price_code = initial_price_code
-  end
-
-  def frequent_renter_points(days_rented)
-    @price.frequent_renter_points(days_rented)
-  end
-
-  def charge(days_rented)
-    @price.charge(days_rented)
+    @price_code = price_code
   end
 end
 
@@ -34,14 +19,6 @@ class Rental
   def initialize(movie, days_rented)
     @movie = movie
     @days_rented = days_rented
-  end
-
-  def charge
-    movie.charge(days_rented)
-  end
-
-  def frequent_renter_points
-    movie.frequent_renter_points(days_rented)
   end
 end
 
@@ -59,74 +36,37 @@ class Customer
   end
 
   def statement
+    total_amount = 0
+		frequent_renter_points = 0
     result = "Rental Record for #{@name}\n"
-    @rentals.each do |rental|
+    @rentals.each do |element|
+      this_amount = 0
+
+      # determine amounts for each line
+      case element.movie.price_code
+      when Movie::REGULAR
+        this_amount += 2
+        this_amount += (element.days_rented - 2) * 1.5 if element.days_rented > 2
+      when Movie::NEW_RELEASE
+        this_amount += element.days_rented * 3
+      when Movie::CHILDRENS
+        this_amount += 1.5
+        this_amount += (element.days_rented - 3) * 1.5 if element.days_rented > 3
+      end
+
+      # add frequent renter points
+      frequent_renter_points += 1
+      # add bonus for a two day new release rental
+      if element.movie.price_code == Movie::NEW_RELEASE && element.days_rented > 1
+        frequent_renter_points += 1
+      end
       # show figures for this rental
-      result += "\t#{rental.movie.title}\t#{rental.charge}\n"
+      result += "\t" + element.movie.title + "\t" + this_amount.to_s + "\n"
+      total_amount += this_amount
     end
     # add footer lines
-    result += "Amount owed is #{total_charge}\n"
-    result += "You earned #{total_frequent_renter_points} frequent renter points"
-    result
-  end
-
-  def html_statement
-    result = "<h1>Rental Record for <em>#{@name}</em></h1>\n"
-    @rentals.each do |rental|
-      # show figures for this rental
-      result += "\t#{rental.movie.title}\t#{rental.charge}<br>\n"
-    end
-    # add footer lines
-    result += "<p>Amount owed is <em>#{total_charge}</em></p>\n"
-    result += "<p>You earned <em>#{total_frequent_renter_points}</em> frequent renter points.</p>"
-    result
-  end
-
-  def total_charge
-    @rentals.inject(0) { |sum, rental| sum + rental.charge }
-  end
-
-  def total_frequent_renter_points
-    @rentals.inject(0) { |sum, rental| sum + rental.frequent_renter_points }
-  end
-end
-
-# module DefaultPrice
-module DefaultPoints
-  def frequent_renter_points(_days_rented)
-    1
-  end
-end
-
-# class RegularPrice
-class RegularPrice
-  include DefaultPoints
-
-  def charge(days_rented)
-    result = 2
-    result += (days_rented - 2) * 1.5 if days_rented > 2
-    result
-  end
-end
-
-# class NewReleasePrice
-class NewReleasePrice
-  def charge(days_rented)
-    days_rented * 3
-  end
-
-  def frequent_renter_points(days_rented)
-    days_rented > 1 ? 2 : 1
-  end
-end
-
-# class ChildrensPrice
-class ChildrensPrice
-  include DefaultPoints
-
-  def charge(days_rented)
-    result = 1.5
-    result += (days_rented - 3) * 1.5 if days_rented > 3
+    result += "Amount owed is #{total_amount}\n"
+    result += "You earned #{frequent_renter_points} frequent renter points"
     result
   end
 end
